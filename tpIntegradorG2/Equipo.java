f3915b9c9db284ec49540aa7e82705e764daf98d
GabrielAballay25 <cdgabrielaballay99@gmail.com>
2023-03-19 23:32:30 -0300

    Create Equipo.java

---
+++ tpIntegradorG2/Equipo.java
@@ -0,0 +1,29 @@
+package tpIntegradorG2;
+import java.time.LocalDate;
+public class Equipo {
+    //Atributos
+    private String nombre;
+    private String descripcion;
+
+    public Equipo (String nombre,String descripcion) {
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+    //metodos
+    public void setNombre(String nombre){
+        this.nombre = nombre;
+    }
+    public String getNombre(){
+        return this.nombre;
+    }
+    public void setDescripcion(String descripcion){
+        this.descripcion = descripcion;
+    }
+    public String getDescripcion(){
+        return this.descripcion;
+    }
+
+    public String toString(){
+        return "Equipo [nombre= "+ nombre + "Descripcion =" + descripcion + "]";
+    }
+}
